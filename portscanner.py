c1c64fe66750194c34b41adb9ef9d5c8b9c978d7
Kabir Bose <kabirbose04@gmail.com>
2024-04-27 11:40:24 -0400

    complete

---
+++ portscanner.py
@@ -0,0 +1,24 @@
+import socket
+
+def scan(target, ports):
+    for port in range(1, ports):
+        scan_port(target, port)
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print("[+] Port open " + str(port))
+        sock.close()
+    except:
+        pass
+
+targets = input("[*] Enter targets to scan (split them by ,): ")
+ports = int(input("[*] Enter how many ports you want to scan: "))
+
+if ',' in targets:
+    print("[*] Scanning multiple targets")
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(' '), ports)
+else:
+    scan(targets, ports)
